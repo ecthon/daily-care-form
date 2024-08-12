72b044dd9c12f8fde8e1d637265c642fc795cddc
ecthon <ecthon@gmail.com>
2024-08-12 11:41:41 -0400

    feature: add checkbox form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,155 @@
-import Image from "next/image";
+'use client'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod"
+import { Checkbox } from "@/components/ui/checkbox"
+
+const items = [
+  { id: "espontanea", label: "Espontânea"},
+  { id: "svd", label: "SVD"},
+  { id: "cistostomia", label: "Cistostomia"},
+  { id: "anurica", label: "Anúrica"},
+  { id: "oligurica", label: "Oligúrica"},
+  { id: "outro", label: "Outro"},
+]
+
+const FormSchema = z.object({
+  username: z.string().min(3),
+  admhospitaluti: z.string().min(3),
+  diagnostico: z.string().min(3),
+  items: z.array(z.string()).refine((value) => value.some((item) => item), {
+    message: "You have to select at least one item.",
+  }),
+})
+
 
 export default function Home() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      items: ["",""]
+    }
+  })
+
+  function onSubmitCheck(data: z.infer<typeof FormSchema>) {
+    // toast({
+    //   title: "You submitted the following values:",
+    //   description: (
+    //     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+    //       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+    //     </pre>
+    //   ),
+    // })
+    console.log(JSON.stringify(data, null, 2))
+  }
+
+  function onSubmit(values: z.infer<typeof FormSchema>) {
+    console.log(values)
+  }
+
   return (
     <div className="w-full h-screen">
-      <h1>hello</h1>
+      <div className="w-[600px] mx-auto py-20 px-10">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Name" {...field} />
+                  </FormControl>
+                  {/* <FormDescription>This is your display name.</FormDescription> */}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="admhospitaluti"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Adm Hosp/UTI</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Name" {...field} />
+                  </FormControl>
+                  {/* <FormDescription>This is your display name.</FormDescription> */}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="diagnostico"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Diagnostico</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Name" {...field} />
+                  </FormControl>
+                  {/* <FormDescription>This is your display name.</FormDescription> */}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="diagnostico"
+              render={({ field }) => (
+                <FormItem>
+              <div className="mb-4">
+                <FormLabel className="text-base">Diurese</FormLabel>
+                <FormDescription>
+                  Select the items you want to display in the sidebar.
+                </FormDescription>
+              </div>
+              {items.map((item) => (
+                <FormField
+                  key={item.id}
+                  control={form.control}
+                  name="items"
+                  render={({ field }) => {
+                    return (
+                      <FormItem
+                        key={item.id}
+                        className="flex flex-row items-start space-x-3 space-y-0"
+                      >
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value?.includes(item.id)}
+                            onCheckedChange={(checked) => {
+                              return checked
+                                ? field.onChange([...field.value, item.id])
+                                : field.onChange(
+                                    field.value?.filter(
+                                      (value) => value !== item.id
+                                    )
+                                  )
+                            }}
+                          />
+                        </FormControl>
+                        <FormLabel className="text-sm font-normal">
+                          {item.label}
+                        </FormLabel>
+                      </FormItem>
+                    )
+                  }}
+                />
+              ))}
+              <FormMessage />
+            </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
